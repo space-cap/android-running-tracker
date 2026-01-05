8fd32980dfd729b3a4bf4e76f74cde6bed7a3d46
이영현 <hong.git@yahoo.com>
2026-01-05 20:08:22 +0900

    feat: 초기 Android 앱 구조와 MainActivity를 생성합니다.

--- app/src/main/java/com/ezlevup/runningtracker/presentation/home/HomeState.kt
+++
@@ -1,5 +0,0 @@
-package com.ezlevup.runningtracker.presentation.home
-
-data class HomeState(
-    val isLoading: Boolean = false,
-)
--- app/src/main/java/com/ezlevup/runningtracker/util/Screen.kt
+++
@@ -1,6 +0,0 @@
-package com.ezlevup.runningtracker.util
-
-sealed class Screen(val route: String) {
-    object Home : Screen("home_screen")
-    object History : Screen("history_screen")
-}
